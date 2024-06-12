61fd7f9443773f7a9d2be19bcc87ffad128b5d8c
chien <john.doe@example.com>
2024-06-12 13:45:14 +0700

    first commit

---
+++ src/main/java/com/example/kiemtragiuaky_nhom20/KiemtragiuakyNhom20Application.java
@@ -0,0 +1,13 @@
+package com.example.kiemtragiuaky_nhom20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiemtragiuakyNhom20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiemtragiuakyNhom20Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kiemtragiuaky_nhom20/KiemtragiuakyNhom20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kiemtragiuaky_nhom20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiemtragiuakyNhom20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
